5afe00321c5c7ceb7214761e292a78409b785bab
Silverstone21 <166360936+Silverstone21@users.noreply.github.com>
2025-08-31 00:54:40 +0530

    Update main.cpp

--- MazeSolver/src/main.cpp
+++ MazeSolver/src/main.cpp
@@ -1,18 +1,17 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// Define pins
+const int trigPin = 2;
+const int echoPin = 3;
+
+// Variables for duration and distance
+long duration;
+float distance;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  
 }
