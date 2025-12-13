56226794c5a6a57728f93b823907d7b575fd81e1
pramodyasahane <pramodyasahan.edu@gmail.com>
2025-12-13 16:39:49 +0530

    Chore: temporarily remove modular files during line-follow L-turn debugging

--- MazeSolver/include/LineFollow.h
+++
@@ -1,8 +0,0 @@
-// LineFollow.h
-#pragma once
-#include "Config.h"
-#include "Hardware.h"
-
-void initLineFollow();
-void lineFollowUpdate();   // call repeatedly in line-follow state
-bool lineReachedEnd();     // white-box at end of line
